f6ca84b626a666387b5fefd7a676694cc4deab8a
jack456054 <jack456054@hotmail.com>
2019-08-11 23:18:40 +0800

    Authentication and logging in

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,24 +1,32 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+# Should be invisible
+app.secret_key = 'jose'
 api = Api(app)
+
+jwr = JWT(app, authenticate, identity)  # /auth
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         # for item in items:
         #     if item['name'] == name:
         #         return item  # We no longer need jsonify because "flask_restful" did for us
 
         # If "next" didn't find anything, return None
-        item = next(filter(lambda x: x[name] == name, items), None)
+        item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        if next(filter(lambda x: x[name] == name, items), None):
+        if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
         data = request.get_json()  # (force=True, silent=True)
