37366ac65f93b98a8f866c21dc59ab526fdd2491
jack456054 <jack456054@hotmail.com>
2019-08-12 21:33:42 +0800

    Finish section 4

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -15,6 +15,9 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True, help="This field cannot be left blank!")
+
     @jwt_required()
     def get(self, name):
         # for item in items:
@@ -29,7 +32,9 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
-        data = request.get_json()  # (force=True, silent=True)
+        data = Item.parser.parse_args()
+        # data = request.get_json()  # (force=True, silent=True)
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         # Status code: 201 is for 'created', 200 is server is ok, 202 is accepted(but maybe delay)
@@ -41,7 +46,9 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        data = Item.parser.parse_args()
+        # data = request.get_json()
+
         item = next(filter(lambda x: x['name'] == name, items), None)
         if not item:
             item = {'name': name, 'price': data['price']}
