d7c3e52ed9dfb5b8b5848f72a494c43e93b31e99
jack456054 <jack456054@hotmail.com>
2019-08-13 15:24:32 +0800

    SQLite test

---
+++ section5/code/app.py
@@ -0,0 +1,70 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+
+app = Flask(__name__)
+# Should be invisible
+app.secret_key = 'jose'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+items = []
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True, help="This field cannot be left blank!")
+
+    @jwt_required()
+    def get(self, name):
+        # for item in items:
+        #     if item['name'] == name:
+        #         return item  # We no longer need jsonify because "flask_restful" did for us
+
+        # If "next" didn't find anything, return None
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
+        data = Item.parser.parse_args()
+        # data = request.get_json()  # (force=True, silent=True)
+
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        # Status code: 201 is for 'created', 200 is server is ok, 202 is accepted(but maybe delay)
+        return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        # data = request.get_json()
+
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if not item:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
+# http://127.0.0.1:5000/item/headphone
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)  # 5000 is the default port
---
+++ section5/code/user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, _id, username, password):
+        # id is a Python keyword
+        self.id = _id
+        self.username = username
+        self.password = password
